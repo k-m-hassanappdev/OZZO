c7397a0fdac71388f46e0a7bcacf29760bae04a6
ASUS <hasanahmedshantu2580@gmail.com>
2025-11-18 09:00:57 +0600

    fragment , navhost , mvvm etc added

--- app/src/main/java/com/example/ozzo/views/starter/MainActivity.kt
+++ app/src/main/java/com/example/ozzo/views/starter/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.ozzo
+package com.example.ozzo.views.starter
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ozzo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
